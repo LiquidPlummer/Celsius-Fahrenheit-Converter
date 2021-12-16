ea39b72a538bc697d83c0d9655f5939532e2c4c4
Kyle <Kyle.Plummer@revature.com>
2021-12-16 17:10:44 -0500

    better version

--- celsius-farenheit-converter/src/converter/converter.js
+++ celsius-farenheit-converter/src/converter/converter.js
@@ -8,15 +8,10 @@ export default function Converter() {
     const [temp, setTemp] = useState({farenheit: 0.0, celsius: 0.0});
 
     const unsubscribe = store.subscribe(() => {
-        if(store.getState().updated == "UPDATE_FARENHEIT") {
-            //Convert F to C and update C
-            setTemp({farenheit: temp.farenheit, celsius: (store.getState().fTemp-32)*(5/9)});
-        }
-
-        if(store.getState().updated == "UPDATE_CELSIUS") {
-            //Convert C to F and update F
-            setTemp({farenheit: store.getState().cTemp * (9/5) + 32, celsius: temp.celsius});
-        }
+        setTemp({
+            celsius: store.getState().cTemp,
+            farenheit: store.getState().fTemp
+        });
     })
 
     return (
